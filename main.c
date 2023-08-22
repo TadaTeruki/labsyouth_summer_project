19158ab9655ebc4274da4164bba2c5ec087f6002
Teruki TADA <tadateruki.public@gmail.com>
2023-08-22 14:04:22 +0900

    [feat] レンダラーの導入まで

--- main.c
+++ main.c
@@ -3,8 +3,12 @@
 #include <stdlib.h>
 #include <wayland-server.h>
 #include <wlr/backend.h>
+#include <wlr/render/allocator.h>
+#include <wlr/render/wlr_renderer.h>
 #include <wlr/types/wlr_keyboard.h>
 #include <wlr/types/wlr_output.h>
+#include <wlr/types/wlr_output_layout.h>
+#include <wlr/types/wlr_scene.h>
 #include <xkbcommon/xkbcommon.h>
 
 // waylandサーバー
@@ -14,6 +18,14 @@ struct morning_server {
     struct wl_display *display;
     // デバイスとの入出力を抽象化 (DRM, libinput, X11など)
     struct wlr_backend *backend;
+    // レンダリングを抽象化 (Pixman, Vulkan, OpenGLなど)
+    struct wlr_renderer *renderer;
+    // レンダリング用のバッファを確保
+    struct wlr_allocator *allocator;
+    // 複数の出力デバイス(モニター)の物理的な位置関係を管理
+    struct wlr_output_layout *output_layout;
+    // scene graphを管理
+    struct wlr_scene *scene;
 
     // 新しい入力デバイスの接続を検知するlistener
     struct wl_listener new_input;
@@ -24,6 +36,23 @@ struct morning_server {
     struct wl_list keyboards;
     // 出力デバイス(モニター)のリスト
     struct wl_list outputs;
+};
+
+// 出力デバイスに関するイベントの情報を保持する構造体
+struct morning_output {
+
+    // waylandサーバー
+    struct morning_server *server;
+    // 出力デバイスの情報
+    struct wlr_output *wlr_output;
+
+    // フレームの更新を検知するlistener
+    struct wl_listener frame;
+    // サーバーの終了を検知するlistener
+    struct wl_listener destroy;
+
+    // リスト用
+    struct wl_list link;
 };
 
 // キーボードに関するイベントの情報を保持する構造体
@@ -87,7 +116,7 @@ static void handle_keyboard_input(struct wl_listener *listener, void *data) {
     }
 }
 
-// キーボードの接続が切れたときのイベント
+// サーバーの終了を検知したときのキーボードに関するイベント
 static void handle_keyboard_destroy(struct wl_listener *listener, void *data) {
     struct morning_keyboard *keyboard = wl_container_of(listener, keyboard, destroy);
     printf("detected keyboard destroy\n");
@@ -140,6 +169,46 @@ static void new_input_notify(struct wl_listener *listener, void *data) {
     }
 }
 
+// フレームの更新イベント
+static void handle_output_frame(struct wl_listener *listener, void *data) {
+
+    // outputのイベント管理用構造体を取得
+    struct morning_output *output = wl_container_of(listener, output, frame);
+
+    // sceneを取得
+    struct wlr_scene *scene = output->server->scene;
+
+    // 特定のoutputに対するsceneを取得
+    struct wlr_scene_output *scene_output = wlr_scene_get_scene_output(scene, output->wlr_output);
+
+    // sceneの内容をoutputにコミット
+    wlr_scene_output_commit(scene_output, NULL);
+
+    // レンダリングの検証用に、画面全体を塗りつぶす
+    wlr_output_attach_render(output->wlr_output, NULL);
+    wlr_renderer_begin(output->server->renderer, output->wlr_output->width, output->wlr_output->height);
+    wlr_renderer_clear(output->server->renderer, (float[]){0.35, 0.45, 0.45, 1});
+    wlr_renderer_end(output->server->renderer);
+    wlr_output_commit(output->wlr_output);
+
+    // 今の時間を取得
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+
+    // frameを更新し、タイムスタンプ
+    wlr_scene_output_send_frame_done(scene_output, &now);
+}
+
+// サーバーの終了を検知したときの出力デバイスに関するイベント
+static void handle_output_destroy(struct wl_listener *listener, void *data) {
+    printf("detected output destroy\n");
+    struct morning_output *output = wl_container_of(listener, output, destroy);
+    wl_list_remove(&output->frame.link);
+    wl_list_remove(&output->destroy.link);
+    wl_list_remove(&output->link);
+    free(output);
+}
+
 // 新しい出力デバイスが接続された時のイベント
 static void new_output_notify(struct wl_listener *listener, void *data) {
 
@@ -148,6 +217,9 @@ static void new_output_notify(struct wl_listener *listener, void *data) {
 
     // outputのデータを取得
     struct wlr_output *wlr_output = data;
+
+    // outputのレンダリングサブシステムを初期化
+    wlr_output_init_render(wlr_output, server->allocator, server->renderer);
 
     // outputの状態(state)を格納する構造体を初期化
     struct wlr_output_state state;
@@ -162,15 +234,32 @@ static void new_output_notify(struct wl_listener *listener, void *data) {
         wlr_output_state_set_mode(&state, mode);
     }
 
+    // outputをoutput_layoutの管理下に追加
+    wlr_output_layout_add_auto(server->output_layout, wlr_output);
+
+    // outputのイベント管理用構造体を作成
+    struct morning_output *output = calloc(1, sizeof(struct morning_output));
+    output->server = server;
+    output->wlr_output = wlr_output;
+
+    // outputsに追加
+    wl_list_insert(&server->outputs, &output->link);
+
+    // outputのイベントを指定
+    output->frame.notify = handle_output_frame;
+    wl_signal_add(&wlr_output->events.frame, &output->frame);
+    output->destroy.notify = handle_output_destroy;
+    wl_signal_add(&wlr_output->events.destroy, &output->destroy);
+
     // outputの状態をコミット
-    // これにより初めて、実際のディスプレイ上に何かが映し出される
+    // これにより初めて、ディスプレイ上に何かが映し出される
     wlr_output_commit_state(wlr_output, &state);
     wlr_output_state_finish(&state);
 }
 
 int main(int argc, char *argv[]) {
 
-    // サーバをー初期化
+    // サーバーを初期化
     struct morning_server server = {0};
 
     // waylandディスプレイを作成
@@ -180,6 +269,23 @@ int main(int argc, char *argv[]) {
     // backendを作成
     server.backend = wlr_backend_autocreate(server.display, NULL);
     assert(server.backend);
+
+    // rendererを作成
+    server.renderer = wlr_renderer_autocreate(server.backend);
+    assert(server.renderer);
+
+    // allocatorを作成
+    server.allocator = wlr_allocator_autocreate(server.backend, server.renderer);
+    assert(server.allocator);
+
+    // output_layoutを作成
+    server.output_layout = wlr_output_layout_create();
+
+    // sceneを作成
+    server.scene = wlr_scene_create();
+
+    // sceneとoutput_layoutを対応付ける
+    wlr_scene_attach_output_layout(server.scene, server.output_layout);
 
     // 出力デバイスのリストを作成
     wl_list_init(&server.outputs);
@@ -207,6 +313,8 @@ int main(int argc, char *argv[]) {
     wl_display_run(server.display);
 
     // 終了
+    wlr_scene_node_destroy(&server.scene->tree.node);
+    wlr_output_layout_destroy(server.output_layout);
     wl_display_destroy(server.display);
 
     return 0;
